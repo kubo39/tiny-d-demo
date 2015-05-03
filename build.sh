304fd6e9221a749f4c187abdf843ba1a1ff1170f
kubo39 <kubo39@gmail.com>
2015-05-04 02:41:27 +0900

    Initial commit

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+for d in dmd nasm; do
+    which $d >/dev/null || (echo "Can't find $d, needed to build"; exit 1)
+done
+
+dmd | head -1
+echo
+
+set -x
+
+dmd -c -noboundscheck -release tinybin.d
+gcc tinybin.o -o payload -e _Dmain -T script.ld -Xlinker --gc-section -l:libphobos2.a -lpthread
+objcopy -j combined -O binary payload payload.bin
+ENTRY=$(nm -f posix payload | grep '_Dmain' | awk '{print $3}')
+nasm -f bin -o tinybin -D entry=0x$ENTRY elf.s
+chmod +x tinybin
+hd tinybin
+wc -c tinybin
---
+++ tinybin.d
@@ -0,0 +1,31 @@
+@system:
+
+void write(size_t p, size_t len)
+{
+  asm
+  {
+    mov RAX, 1;  // WRITE
+    mov RDI, 1;  // STDOUT
+    mov RSI, p[RBP];
+    mov RDX, len[RBP];
+    syscall;
+  }
+}
+
+void exit()
+{
+  asm
+  {
+    mov RAX, 60;  // EXIT
+    mov RDI, 0;
+    syscall;
+  }
+}
+
+
+void main()
+{
+  immutable(char)[7] buf = "Hello!\n";
+  write(cast(size_t) buf.ptr, 7);
+  exit();
+}
