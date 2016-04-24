1d9cb28a94e47df9235e4219f37dadfe2c102cb1
kubo39 <kubo39@gmail.com>
2016-04-25 07:09:02 +0900

    Change indent: 2spaces -> 4spaces.

--- tinybin.d
+++ tinybin.d
@@ -1,15 +1,15 @@
 extern (C) void main()
 {
-  asm
-  {
-    naked;
-    mov RAX, 1;  // WRITE
-    mov RDI, 1;  // STDOUT
-    mov RSI, 0x400008;
-    mov RDX, 7;
-    syscall;
-    mov RAX, 60;  // EXIT
-    xor RDI, RDI;
-    syscall;
-  }
+    asm
+    {
+        naked;
+        mov RAX, 1;  // WRITE
+        mov RDI, 1;  // STDOUT
+        mov RSI, 0x400008;
+        mov RDX, 7;
+        syscall;
+        mov RAX, 60;  // EXIT
+        xor RDI, RDI;
+        syscall;
+    }
 }
